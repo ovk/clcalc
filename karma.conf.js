1130de13808d64ac2601cd485ebf1eafbce829a6
ovk <mail@okosh.xyz>
2020-06-25 21:26:13 -0400

    Use headless Firefox for tests

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config)
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: false,
-        browsers: ['ChromeHeadlessNoSandbox', 'Firefox' ],
+        browsers: ['ChromeHeadlessNoSandbox', 'FirefoxHeadless' ],
         customLaunchers:
         {
             ChromeHeadlessNoSandbox:
